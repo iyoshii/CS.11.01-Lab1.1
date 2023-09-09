546fce8c9a0091ec2418092e0dbd5bddbef06688
yoshi <puggiedountsme@gmail.com>
2023-09-09 13:34:16 +0800

    Update 1.0

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,14 @@
-public class Dateable {
+import java.util.Scanner;
 
+public class Dateable {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your age: ");
+        int age = scanner.nextInt();
+
+        int minAge = 7+(age/2);
+
+        System.out.println(age+"-year olds should date somebody who is at least "+minAge+" years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Please enter your age: ");
+        int age = scanner.nextInt();
+
+        int minPages = 100-age;
+
+        System.out.println(age+"-year olds should read at least "+minPages+" pages before giving up on a book.");
     }
 }
